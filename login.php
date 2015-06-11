408f07f88db2b75e19b7bd5e5e9e7d3ff4754af0
Jinder Wang <jinder.wang@gmail.com>
2015-06-12 00:08:28 +0800

    add sessoin

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+include_once('sessionconfig.php');
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login System</title>
+</head>
+
+<body>
+<div align="center">
+
+</div>
+</body>
+</html>
